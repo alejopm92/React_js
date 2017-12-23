8f9a28b9a941c58327afb52b7acd401e2b439a68
Sergio Alejandro Espitia <alejo.pm92@gmail.com>
2017-12-22 22:20:40 -0500

    [update] discovering the black arts of arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,90 +1,29 @@
 'use strict';
 
-var myP = 'This is my New Change JSX Write it by myself LOLZ';
-var myTittle = 'Indecision App Dinamic Tittle';
-
-var whishList = {
-  title: 'This is the list of my propurse for 2018:',
-  l1: 'React SR',
-  l2: 'Travel A LOT',
-  l3: 'PIMP BETTER' };
-
-var options = {
-  opt1: 'Buy a DSLR CAM',
-  opt2: 'Get a lot better',
-  opt3: 'Be a better soft developer' };
-
-function betterList() {
-  if (options) {
-    return React.createElement(
-      'ul',
-      null,
-      React.createElement(
-        'h4',
-        null,
-        'Better things to do 2018'
-      ),
-      Object.keys(options).map(function (opt, index) {
-        return React.createElement(
-          'li',
-          { key: index },
-          options[opt]
-        );
-      })
-    );
-  }
+function squ(x) {
+  return x * x;
 };
 
-function getList() {
-  if (Object.keys(whishList).length > 0) {
-    return Object.keys(whishList).map(function (key, index) {
-      return React.createElement(
-        'li',
-        { key: index },
-        whishList[key]
-      );
-    });
-  }
+console.log(squ(5));
+
+var square = function square(x) {
+  return x * x;
 };
 
-function getTitle(opt) {
-  if (opt) {
-    return React.createElement(
-      'h4',
-      null,
-      whishList.title
-    );
-  } else {
-    return React.createElement(
-      'h5',
-      null,
-      'Nonsense'
-    );
-  }
-}
+console.log(square(25));
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    myTittle
-  ),
-  React.createElement(
-    'p',
-    null,
-    myP
-  ),
-  React.createElement(
-    'ol',
-    null,
-    whishList.title ? getTitle('lol') : getTitle(),
-    getList()
-  ),
-  betterList()
-);
+var squareSimple = function squareSimple(x) {
+  return x * x;
+};
 
-var appRoot = document.getElementById('app');
+console.log(square(35));
 
-ReactDOM.render(template, appRoot);
+var getFirstName = function getFirstName(name) {
+  return name.split(' ')[0];
+};
+console.log(getFirstName('Luis Fernando'));
+
+var getFirstNameOther = function getFirstNameOther(name) {
+  return name.split(' ')[0];
+};
+console.log(getFirstNameOther('Sergio Alejandro'));
