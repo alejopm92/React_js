021b45ccb2409385cc796f43dc3108754aab1b9b
Sergio Alejandro Espitia <alejo.pm92@gmail.com>
2017-12-26 10:36:56 -0500

    [udpate] javaScript POO

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,48 +1,39 @@
 'use strict';
 
-var appRoot = document.getElementById('app');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var showDetails = false;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var visibility = function visibility() {
-  showDetails = !showDetails;
-  reRenderApp();
-};
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'None Sense';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var showedDetails = function showedDetails() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'button',
-      { onClick: visibility },
-      showDetails ? 'Hide Details' : 'Show Details'
-    ),
-    showDetails && React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'p',
-        null,
-        'Hey my algorithmia Sucks, and here are more details that I want you to know!'
-      )
-    )
-  );
-};
+    _classCallCheck(this, Person);
 
-var reRenderApp = function reRenderApp() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Visible Toogle'
-    ),
-    showedDetails()
-  );
+    this.name = name;
+    this.age = age;
+  }
 
-  ReactDOM.render(template, appRoot);
-};
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi I am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' years(s) old';
+    }
+  }]);
 
-reRenderApp();
+  return Person;
+}();
+
+var me = new Person('Alejandro Espitia', 25);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,22 @@
+
+class Person {
+  constructor(name = 'None Sense', age =0 ) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getGreeting () {
+    return `Hi I am ${this.name}!`;
+  }
+  getDescription () {
+    return `${this.name} is ${this.age} years(s) old`;
+  }
+}
+
+const me = new Person('Alejandro Espitia',25);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getGreeting());
+console.log(other.getDescription());
