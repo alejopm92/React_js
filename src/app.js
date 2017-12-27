da782c5c2d5c7a1f269528533f0319ebd9c678ce
Sergio Alejandro Espitia <alejo.pm92@gmail.com>
2017-12-26 20:24:14 -0500

    [update] state props

--- src/app.js
+++ src/app.js
@@ -1,15 +1,62 @@
 
 class IndecisionApp extends React.Component {
+  constructor (props){
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handleAction = this.handleAction.bind(this);
+    this.state = {
+      options_render: []
+    };
+  }
+
+  handleAction() {
+    const rand_num = Math.floor(this.state.options_render.length * Math.random());
+    const action_selected = this.state.options_render[rand_num]
+    alert(action_selected);
+  }
+
+  handleAddOption(new_option){
+
+    if(!new_option){
+      return 'Enter a valid option.';
+    }
+    else if (this.state.options_render.indexOf(new_option) > -1){
+      return 'This option already exists.';
+    }
+
+    this.setState((prevState) => {
+      return {
+        options_render : prevState.options_render.concat(new_option)
+      };
+    });
+  }
+
+  handleDeleteOptions (){
+    alert('All options are going to be deleted');
+    this.setState(() => {
+      return {
+        options_render: []
+      };
+    });
+  }
   render() {
     const title = 'Indecision Test';
-    const subtitle = 'Take Action!'
-    const options_render = ['Opt to tke 1', 'Opt to tke 2', 'Opt to tke 3']
+    const subtitle = 'Take Action!';
     return (
         <div>
           <Header title={title} subtitle={subtitle}/>
-          <Action />
-          <Options options={options_render} />
-          <AddOption />
+          <Action
+            hasOptions={this.state.options_render.length > 0}
+            handleAction={this.handleAction}
+          />
+          <Options
+            options={this.state.options_render}
+            handleDeleteOptions={this.handleDeleteOptions}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
       </div>
     );
   }
@@ -31,7 +78,11 @@ class Action extends React.Component {
   render() {
     return (
       <div>
-        <button>What is the best decision to take?</button>
+        <button
+          onClick={this.props.handleAction}
+          disabled={!this.props.hasOptions}>
+          What is the best decision to take?
+        </button>
       </div>
     );
   }
@@ -48,19 +99,11 @@ class Option extends React.Component {
 }
 
 class Options extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind (this);
-  }
-  handleRemoveAll () {
-    if(this.props.options){
-        alert('All options were remove');
-    }
-  }
+
   render () {
     return (
       <div>
-        <button onClick={this.handleRemoveAll.bind(this)}>Remove All options</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All options</button>
         <p>Here should be some options for render like:</p>
         <ul>
           {this.props.options.map((opt, index) => (
@@ -73,19 +116,32 @@ class Options extends React.Component {
 }
 
 class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state ={
+      error: undefined
+    };
+  }
   handleAddOption (e) {
     e.preventDefault();
     const new_option = e.target.elements.option_add.value.trim();
-    if (new_option) {
-      alert(new_option);
-    }
+    const error = this.props.handleAddOption(new_option);
+
+    this.setState(() =>{
+      return {error};
+      }
+    );
   }
   render(){
     return (
-      <form onSubmit={this.handleAddOption}>
-        <input type='text' name='option_add' />
-        <button>This should add an Option for render</button>
-      </form>
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type='text' name='option_add' />
+          <button>This should add an Option for render</button>
+        </form>
+      </div>
     );
   }
 }
